83b00fcea4c8df0511fbe1c410eeda6a94aa1713
HAN JONGBEEN <stompesi@gmail.com>
2018-11-22 23:50:04 +0900

    Apply promise

--- app.ts
+++ app.ts
@@ -42,30 +42,33 @@ function httpRequestCallback(response: any){
             transactionsRoot : block.transactionsRoot,
             extraData : block.extraData,
             sha3Uncles : block.sha3Uncles
+        }).then((results) => {
+            for(let i = 0 ; i < block.transactions.length ; i ++) {
+                Transaction.create({
+                    transactionHash : block.transactions[i].hash,
+                    blockHash : block.transactions[i].blockHash,
+                    from : block.transactions[i].from,
+                    to : block.transactions[i].to,
+                    input : block.transactions[i].input,
+                    gas : block.transactions[i].gas,
+                    gasPrice : block.transactions[i].gasPrice,
+                    nonce : block.transactions[i].nonce,
+                    transactionIndex : block.transactions[i].transactionIndex,
+                    value : block.transactions[i].value,
+                    v : block.transactions[i].v,
+                    r : block.transactions[i].r,
+                    s : block.transactions[i].s
+                }).then((results) => {
+                }).catch((err) => {
+                });
+            }
+            console.log(parseInt(block.number, 16));
+        }).catch((err) => {
         });
-
-        for(let i = 0 ; i < block.transactions.length ; i ++) {
-            Transaction.create({
-                transactionHash : block.transactions[i].hash,
-                blockHash : block.transactions[i].blockHash,
-                from : block.transactions[i].from,
-                to : block.transactions[i].to,
-                input : block.transactions[i].input,
-                gas : block.transactions[i].gas,
-                gasPrice : block.transactions[i].gasPrice,
-                nonce : block.transactions[i].nonce,
-                transactionIndex : block.transactions[i].transactionIndex,
-                value : block.transactions[i].value,
-                v : block.transactions[i].v,
-                r : block.transactions[i].r,
-                s : block.transactions[i].s
-            });
-        }
-        console.log(parseInt(block.number, 16));
    });
 }
 
-cron.schedule('*/2 * * * * *', function(){
+cron.schedule('*/2 * * * * *', () => {
 	for(let i = 0 ; i < 5 ; i++) {
         options.path = `/api?module=proxy&action=eth_getBlockByNumber&boolean=true&tag=${blockNumber.toString(16)}`
 
